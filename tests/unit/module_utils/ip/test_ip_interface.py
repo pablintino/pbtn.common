003a5359547522bd261bef58711b2c2595d27f4a
Pablo Rodriguez Nava <pablintino@hotmail.com>
2024-02-28 01:31:52 +0100

    Improved ip_interface coverage with some basic UTs

--- tests/unit/module_utils/ip/test_ip_interface.py
+++ tests/unit/module_utils/ip/test_ip_interface.py
@@ -1,6 +1,7 @@
 import typing
 
 from ansible_collections.pablintino.base_infra.plugins.module_utils.ip.ip_interface import (
+    IPAddrData,
     IPLinkData,
     IPInterface,
 )
@@ -96,3 +97,61 @@ def test_ip_interface_get_links_vlan_ok(command_mocker_builder, test_file_manage
     __validate_ether_link(links, "eth2", "d2:55:ee:86:11:24")
     __validate_vlan_link(links, "eth1.20", "52:54:00:40:6c:02")
     __validate_vlan_link(links, "eth2.100", "d2:55:ee:86:11:24")
+
+
+def test_ip_interface_ip_link_data_fields_ok():
+    """
+    Tests that the IPLinkData class is able to
+    access the basic needed fields of an
+    `ip link` element and that the constants to
+    each field point to the proper values.
+    """
+    ip_link_data = IPLinkData(
+        {
+            "ifname": "eth1.20",
+            "link": "eth1",
+            "address": "25:ef:82:13:ec:3c",
+            "linkinfo": {
+                "info_kind": "veth",
+                "info_slave_kind": "bridge",
+                "info_slave_data": {},
+            },
+        }
+    )
+    assert ip_link_data.link == "eth1"
+    assert ip_link_data.if_name == "eth1.20"
+    assert ip_link_data.address == "25:ef:82:13:ec:3c"
+    assert ip_link_data.link_kind == "veth"
+
+    # Test that constants point to the expected values
+    assert IPLinkData.LINK_DETAILS_IFNAME == "ifname"
+    assert IPLinkData.LINK_DETAILS_LINK == "link"
+    assert IPLinkData.LINK_DETAILS_ADDR == "address"
+    assert IPLinkData.LINK_DETAILS_LINK_INFO == "linkinfo"
+    assert IPLinkData.LINK_DETAILS_INFO_KIND == "info_kind"
+
+
+def test_ip_interface_ip_addr_data_fields_ok():
+    """
+    Tests that the IPAddrData class is able to
+    access the basic needed fields of an
+    `ip addr` element and that the constants to
+    each field point to the proper values.
+    """
+
+    # Check that the getters work as expected
+    ip_addr_data = IPAddrData(
+        {
+            "ifname": "eth1.20",
+            "link": "eth1",
+            "addr_info": [{"local": "192.168.122.100"}],
+        }
+    )
+    assert ip_addr_data.addr_info == [{"local": "192.168.122.100"}]
+    assert ip_addr_data.link == "eth1"
+    assert ip_addr_data.if_name == "eth1.20"
+
+    # Test that constants point to the expected values
+    assert IPAddrData.ADDR_DETAILS_ADDR_INFO == "addr_info"
+    assert IPAddrData.ADDR_DETAILS_IFNAME == "ifname"
+    assert IPAddrData.ADDR_DETAILS_LINK == "link"
